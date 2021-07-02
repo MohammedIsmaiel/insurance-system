65c2cbed0c0f3f5a025e52d5e50fddc3dbcb3074
Mohammed_Ismaiel <mohammedismaiel333.z@gmail.com>
2021-07-02 19:00:53 +0200

    some end points was added

--- src/routers/main.js
+++ src/routers/main.js
@@ -1,12 +1,124 @@
 const express = require("express");
 const router = express.Router();
+const { db } = require('./db/dbconnect');
 
 
 router.get("/", (req, res) => {
     return res.json({ message: 'welcome' });
 });
 
+//list all companies
+router.get("/get-all-companies", (req, res) => {
+    try {
+        con.connect(function (err) {
+            if (err) throw err;
+            //TODO #1 insert sql code here
+            var sql = "";
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log(result);
+            });
+        });
+        res.json({
+            allCompanies
+        })
+    } catch (err) {
+        return res.status(400).json({
+            message: err.message
+        });
+    }
+});
+
+//add new company
+router.post("/add-new-company", (req, res) => {
+    try {
+        let newCompany = req.body
+        con.connect(function (err) {
+            if (err) throw err;
+            //TODO #1 insert sql code here
+            var sql = "";
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log(result);
+            });
+        });
+        res.json({
+            newCompany
+        })
+    } catch (err) {
+        return res.status(400).json({
+            message: err.message
+        });
+    }
+});
+
+//add new customer
+router.post("/add-new-Customer", (req, res) => {
+    try {
+        let newCustomer = req.body
+        con.connect(function (err) {
+            if (err) throw err;
+            //TODO #1 insert sql code here
+            var sql = "";
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log(result);
+            });
+        });
+        res.json({
+            newCustomer
+        })
+    } catch (err) {
+        return res.status(400).json({
+            message: err.message
+        });
+    }
+});
 
 
+//search for a customer
+router.get("/get-Customer", (req, res) => {
+    try {
+        con.connect(function (err) {
+            if (err) throw err;
+            //TODO #1 insert sql code here
+            var sql = "";
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log(result);
+            });
+        });
+        res.json({
+            customer
+        })
+    } catch (err) {
+        return res.status(400).json({
+            message: err.message
+        });
+    }
+});
+
+//add new program
+router.post("/add-new-program", (req, res) => {
+    try {
+        let newProgram = req.body
+        con.connect(function (err) {
+            if (err) throw err;
+            //TODO #1 insert sql code here
+            var sql = "";
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log(result);
+            });
+        });
+        res.json({
+            newProgram
+        })
+    } catch (err) {
+        return res.status(400).json({
+            message: err.message
+        });
+    }
+});
 
 module.exports = router
