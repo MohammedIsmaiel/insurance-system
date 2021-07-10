4ecea9bd516b5c3ae35ebe4068dbc2337f9834e7
Mohamed Hassan <mohamed.hassn.dev@gmail.com>
2021-07-10 17:50:21 +0200

    Modify connection type to use aws rds

--- src/db/dbconnect.js
+++ src/db/dbconnect.js
@@ -8,7 +8,6 @@ const db = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DBNAME,
     multipleStatements: true
 });
 
