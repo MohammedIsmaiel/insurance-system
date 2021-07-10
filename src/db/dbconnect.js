f409b4d71b05a871641914ad6caa346270fa72ef
Mohamed Hassan <mohamed.hassn.dev@gmail.com>
2021-07-10 18:11:17 +0200

    Add data base name

--- src/db/dbconnect.js
+++ src/db/dbconnect.js
@@ -8,6 +8,7 @@ const db = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
+    database: process.env.DBNAME,
     multipleStatements: true
 });
 
